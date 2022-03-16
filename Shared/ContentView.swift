f295bfe3653e47476cc7fbca0ae754c58f5d50c2
Hanif Shersy <hanif.shersy@slyp.com.au>
2022-03-16 11:48:54 +1100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Hanif Shersy on 16/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/RaspStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaspStatApp.swift
+//  Shared
+//
+//  Created by Hanif Shersy on 16/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct RaspStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
